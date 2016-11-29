f6e5a60ba0889256a6d45447577672ffaf015439
Antonio Román <alroman@emergya.com>
2016-11-29 20:58:31 +0100

    xml to json object done

--- src/pkjs/index.js
+++ src/pkjs/index.js
@@ -1,10 +1,9 @@
 var tussamApi = require('./tussamapi');
 
-Pebble.addEventListener('ready', 
+Pebble.addEventListener('ready',
   function(e) {
     console.log('PebbleKit JS ready!');
     console.log('Call to getNodosCercanos');
     tussamApi.getNodosCercanos();
   }
-);                  
-
+);
